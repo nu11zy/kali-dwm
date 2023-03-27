915ef2420f4b967256e275e26c2fd29e7255e131
nu11zy <nu11z@yandex.ru>
2023-03-27 23:26:50 +0300

    dwm

---
+++ dwm/patch/include.h
@@ -0,0 +1,27 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_awesomebar.h"
+#include "bar_systray.h"
+#include "bar_wintitleactions.h"
+
+/* Other patches */
+#include "attachx.h"
+#include "cyclelayouts.h"
+#include "pertag.h"
+#include "placemouse.h"
+#include "push_no_master.h"
+#include "scratchpad.h"
+#include "vanitygaps.h"
+#include "warp.h"
+/* Layouts */
+#include "layout_grid.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
