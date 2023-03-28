924f693872cff142cf12bcac89d4e606515d5310
nu11zy <55335158+nu11zy@users.noreply.github.com>
2023-03-28 04:07:19 +0300

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -229,6 +229,7 @@ static const char *dmenucmd[] = {
 	NULL
 };
 static const char *termcmd[]  = { "qterminal", NULL };
+static const char *logoffcmd[]  = { "xfce4-session-logout", NULL };
 
 
 
@@ -236,6 +237,7 @@ static const Key keys[] = {
 	/* modifier                     key            function                argument */
 	{ MODKEY,                       XK_d,          spawn,                  {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return,     spawn,                  {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Esc,        spawn,                  {.v = logoffcmd } },
 	{ MODKEY,                       XK_b,          togglebar,              {0} },
 	{ MODKEY,                       XK_j,          focusstack,             {.i = +1 } },
 	{ MODKEY,                       XK_k,          focusstack,             {.i = -1 } },
