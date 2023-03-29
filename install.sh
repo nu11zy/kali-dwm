06d26432453460836869aacb1dfc73ad7f49a6a1
nu11zy <55335158+nu11zy@users.noreply.github.com>
2023-03-29 17:34:51 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+sudo apt update
+sudo apt install -y build-essential
+
+# install ly
+sudo apt install -y libpam0g-dev libxcb-xkb-dev
+git clone --recurse-submodules https://github.com/fairyglade/ly ~/.local/.desktop/ly
+(cd ~/.local/.desktop/ly && sudo make clean install installsystemd)
+sudo systemctl disable lightdm && sudo systemctl enable ly && test -f ./ly.config && sudo cp ./ly.config /etc/ly/config.ini
+
+# install dwm
+sudo apt install -y libx11-dev libxft-dev libxinerama-dev dmenu
+cp -r ./dwm ~/.local/.desktop/dwm
+(cd ~/.local/.desktop/dwm && sudo make clean install)
+
+# install slstatus
+cp -r ./slstatus ~/.local/.desktop/slstatus
+(cd ~/.local/.desktop/slstatus && sudo make clean install)
+
+# install wallpaper
+sudo apt install -y xwallpaper
+cp wallpaper.png ~/.local/.desktop/
+
+# install st
+git clone git://git.suckless.org/st ~/.local/.desktop/st
+(cd ~/.local/.desktop/st && sudo make clean install)
